6dee5f6a918381c4a7facfe67e191aa95bcf34f7
raycuba <raycubA15@gmail.com>
2025-06-17 12:06:51 +0200

    añadiendo importacion del repositorio en services

--- ddd/management/templates/service/imports.py
+++ ddd/management/templates/service/imports.py
@@ -2,3 +2,4 @@ from typing import List, Optional
 
 # importa las entidades utilizadas aqui
 from .entities import [[ entity_name.capitalize() ]]Entity
+from ..infrastructure.[[ entity_name.lower() ]]_repository import [[ entity_name.capitalize() ]]Repository
