396f347cbee9d888da855e40b6ec50dc8664690d
raycuba <raycubA15@gmail.com>
2025-10-18 13:45:24 +0200

    correccion de importacion de validadores de pydantic

--- ddd/management/templates/dto/imports_pydantic.py
+++ ddd/management/templates/dto/imports_pydantic.py
@@ -1,4 +1,4 @@
 from uuid import UUID
 from datetime import datetime
-from pydantic import BaseModel, ConfigDict, Field, validator
+from pydantic import BaseModel, ConfigDict, Field, validator, model_validator, field_validator
 from typing import Dict, List, Optional, Any
--- ddd/management/templates/entity/imports_pydantic.py
+++ ddd/management/templates/entity/imports_pydantic.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, ConfigDict, Field, validator
+from pydantic import BaseModel, ConfigDict, Field, validator, model_validator, field_validator
 from typing import Optional, List, Dict, Any, ClassVar
 from uuid import UUID
 from datetime import datetime
